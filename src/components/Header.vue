2822f4dca9d070fa40a7253b6f93ac6d16b07c80
Jair Zea Paez <jairzeapaez@MacBook-Air-de-Jair.local>
2023-11-20 11:39:55 -0500

    upload pages

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,14 @@
-<!-- ./src/components/AppHeader.vue -->
 <template>
   <header class="bg-indigo-800 shadow-md">
     <div class="container mx-auto flex items-center justify-between p-4">
-      <!-- Logo -->
+      <!-- logo -->
       <div class="flex items-center">
-        <span class="text-lg font-bold text-white">TalentPitch</span>
+        <span class="text-lg font-bold text-white">Talentpitch</span>
       </div>
 
-      <!-- Menú -->
+      <!-- Menu -->
       <nav class="flex space-x-8">
-        <a href="#" class="text-gray-300 hover:text-purple-500">Conéctate</a>
+        <a href="#" class="text-gray-300 hover:text-purple-500">Cónectate</a>
         <a href="#" class="text-gray-300 hover:text-purple-500">Mis muros</a>
         <a href="#" class="text-gray-300 hover:text-purple-500">Mis listas</a>
         <a href="#" class="text-gray-300 hover:text-purple-500">Aprende</a>
@@ -17,9 +16,9 @@
 
       <!-- Iconos -->
       <div class="flex items-center space-x-4">
-        <!-- Botón Comenzar -->
+        <!-- Boton comenzar -->
         <button
-          class="bg-purple-600 rounded text-white px-4 py-2 flex items-center space-x-1 start-btn"
+          class="secondary rounded text-white px-4 py-2 flex items-center space-x-1 start-btn"
         >
           <span>Comenzar</span>
           <span class="text-lg">+</span>
@@ -29,7 +28,7 @@
             <Message />
           </el-icon>
         </el-tooltip>
-        <el-tooltip content="Notificaciones" placement="bottom">
+        <el-tooltip content="Correo electrónico" placement="bottom">
           <el-icon size="25" color="#ffff">
             <Bell />
           </el-icon>
@@ -38,31 +37,19 @@
         <!-- Circle Avatar con Dropdown -->
         <el-dropdown trigger="hover">
           <el-avatar
-            src="@/assets/images/03b0d39583f48206768a7534e55bcpng.png"
+            src="@/assets/images/avatar.png"
             size="medium"
             :width="30"
             :height="30"
           ></el-avatar>
-          <el-dropdown-menu>
-            <el-dropdown-item>Perfil</el-dropdown-item>
-            <el-dropdown-item>Configuración</el-dropdown-item>
-            <el-dropdown-item divided>Salir</el-dropdown-item>
-          </el-dropdown-menu>
+          <el-dropdown-menu></el-dropdown-menu>
         </el-dropdown>
       </div>
     </div>
   </header>
 </template>
-
 <script>
 export default {
   name: "AppHeader",
 };
 </script>
-
-<style scoped>
-.start-btn {
-  background-color: #de1c7d;
-}
-/* Estilos adicionales según sea necesario */
-</style>
